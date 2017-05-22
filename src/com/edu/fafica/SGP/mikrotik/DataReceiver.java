ecad6e98b34e241c35e587edd4e7a2182f21a729
Dyego Lemos <dyegodll18@gmail.com>
2017-05-22 17:26:45 -0300

    Classe MIKROTIK

---
+++ src/com/edu/fafica/SGP/mikrotik/DataReceiver.java
@@ -0,0 +1,34 @@
+package com.edu.fafica.SGP.mikrotik;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DataReceiver extends Thread{
+
+	 private ApiConn aConn = null;
+	   T3apiView t3A = null;
+	 
+	   public DataReceiver(ApiConn aConn, T3apiView t3A) {
+	       this.aConn = aConn;
+	       this.t3A = t3A;
+	   }
+	 
+	   @Override
+	   public void run() {
+	       String s = "";
+	       while (true) {
+	           try {
+	               s = aConn.getData();
+	               if (s != null) {
+	                   t3A.outputHere(s);
+	                   if (s.contains("!done")) {
+	                   }
+	               }
+	           } catch (InterruptedException ex) {
+	               Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
+	           }
+	       }
+	   }
+	
+}
---
+++ src/com/edu/fafica/SGP/mikrotik/T3apiView.java
@@ -0,0 +1,30 @@
+package com.edu.fafica.SGP.mikrotik;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class T3apiView {
+
+	public void outputHere(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+/*
+    ApiConn ret = new ApiConn("192.168.88.1", 8728);
+    if (!ret.isConnected()) {
+        ret.start();
+        try {
+            ret.join();
+            if (ret.isConnected()) {
+                ret.login("admin", new char[0]);
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(T3apiView.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    aConn.sendCommand("/ip/address/print");
+    DataReceiver dataRec = new DataReceiver(aConn, this);
+    dataRec.start();
+*/	
+}
